34b6bf5df9af7b02e9b402bea5909be4656fe92e
Tomasz Knapik <tomasz.knapik@torchbox.com>
2017-08-23 14:28:40 +0100

    Add links

--- random_redirecter/views.py
+++ random_redirecter/views.py
@@ -5,8 +5,11 @@ from flask import Flask, redirect
 app = Flask(__name__)
 
 REDIRECT_URLS = [
-    'http://dgg.gg/',
-    'http://mozilla.org/',
+    'https://www.loop11.com/usertest/39566/',
+    'https://www.loop11.com/usertest/39564/',
+    'https://www.loop11.com/usertest/39563/',
+    'https://www.loop11.com/usertest/39559/',
+    'https://www.loop11.com/usertest/39500/',
 ]
 
 
